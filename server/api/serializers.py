be594c1e51ba53080946653fae95b7a5865828b7
Hieu Chi Vu <43056834+Hieu1050@users.noreply.github.com>
2021-11-28 19:52:58 +0900

    resolve database

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,14 +1,14 @@
-from rest_framework import serializers
+# from rest_framework import serializers
 
-import api.models as models
+# import api.models as models
 
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Course
-        fields = ['id', 'name', 'lecturers', 'school', 'description', 'date_start', 'date_end', 'is_closed', 'cost']
-        read_only_fields = ['lecturers']
+# class CourseSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = models.Course
+#         fields = ['id', 'name', 'lecturers', 'school', 'description', 'date_start', 'date_end', 'is_closed', 'cost']
+#         read_only_fields = ['lecturers']
 
-class SchoolSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.School
-        fields = '__all__'
+# class SchoolSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = models.School
+#         fields = '__all__'
