21b201a7355422411ac14da523220f2d4badd35e
Thanh Dang <ducthanhdang2512@gmail.com>
2022-01-12 09:31:45 +0900

    Temp

---
+++ client/app/pages/todo/index.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import Board from '@asseinfo/react-kanban'
+import '@asseinfo/react-kanban/dist/styles.css'
+
+const board = {
+    columns: [
+      {
+        id: 1,
+        title: 'Backlog',
+        cards: [
+          {
+            id: 1,
+            title: 'Add card',
+            description: 'Add capability to add a card in a column'
+          },
+        ]
+      },
+      {
+        id: 2,
+        title: 'Doing',
+        cards: [
+          {
+            id: 2,
+            title: 'Drag-n-drop support',
+            description: 'Move a card between the columns'
+          },
+        ]
+      }
+    ]
+  }
+
+const ToDoBoard = () => {
+    return <Board>{board}</Board>
+}
+
+export default ToDoBoard;
